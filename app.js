5f5f064638721fdfb84cd4bc476ae210f879d6a2
jp-ca <jp_ca@ymail.com>
2014-11-02 17:27:51 -0800

    --

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var youtube = require('youtube-node');
 youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
 
 youtube.search('World War z Trailer', 2, function(resultData) {
-
-    // console.log('resultData =' , resultData);
+    console.log('resultData =' , resultData);
     console.log('resultData.items',  resultData.items );    
-
 });
+
+
+youtube.getById('HcwTxRuq-uk', function(result) {
+    // console.log('result =' , result);
+    // console.log('content details =' , result.contentDetails);    
+});
+
+
